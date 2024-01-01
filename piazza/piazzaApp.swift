9a53633a034c8526c08a422b692d45e3c81eca91
mliebich <michi.liebich@gmail.com>
2024-01-01 18:26:34 +0100

    initial

---
+++ piazza/piazzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  piazzaApp.swift
+//  piazza
+//
+//  Created by Michael Liebich on 01.01.24.
+//
+
+import SwiftUI
+
+@main
+struct piazzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
